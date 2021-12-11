713aa23395d4ae18feebb6ffe764da4a87fb081d
kam237zasada <kam237zasada@wp.pl>
2021-12-11 15:07:11 +0100

    add models to db object

--- server/src/DBModels/index.ts
+++ server/src/DBModels/index.ts
@@ -1,6 +1,21 @@
 import { Sequelize } from "sequelize";
 import { createNamespace } from "cls-hooked";
 
+import Answer from "./Answer";
+import Category from "./Category";
+import Course from "./Course";
+import CourseFile from "./CourseFile";
+import CourseStudent from "./CourseStudent";
+import Exam from "./Exam";
+import ExamQuestion from "./ExamQuestion";
+import File from "./File";
+import Grade from "./Grade";
+import Question from "./Question";
+import QuestionAnswer from "./QuestionAnswer";
+import SolvedExam from "./SolvedExam";
+import Student from "./Student";
+import Teacher from "./Teacher";
+
 const cls = createNamespace("namespace");
 Sequelize.useCLS(cls);
 
@@ -19,5 +34,21 @@ export const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER,
 
 db.Sequelize = Sequelize;
 db.sequelize = sequelize;
+
+db.answer = Answer(sequelize);
+db.category = Category(sequelize);
+db.course = Course(sequelize);
+db.courseFile = CourseFile(sequelize);
+db.courseStudent = CourseStudent(sequelize);
+db.exam = Exam(sequelize);
+db.examQuestion = ExamQuestion(sequelize);
+db.file = File(sequelize);
+db.grade = Grade(sequelize);
+db.question = Question(sequelize);
+db.questionAnswer = QuestionAnswer(sequelize);
+db.solvedExam = SolvedExam(sequelize);
+db.student = Student(sequelize);
+db.teacher = Teacher(sequelize);
+
 
 export default db;
